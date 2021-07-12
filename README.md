1cbd6439cea3aa082b18ecc6d279ebe7667f1a3d
deepakandgupta <deepakandgupta@gmail.com>
2021-07-12 17:38:41 +0530

    Plugin v0.0.1

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fetches and then adds existing DynamoDB Table streams to serverless.yml file using table name for serverless framework
+
+This plugin calls AWS Describe Streams on you behalf and adds the stream to serverless config file.
+
+## How to use
+
+### 1. Add to your serverless config plugins object
+
+### 2. Call the function on your stream arn key like below
+
+##### arn: ${fetchStreamARN(<tableName>)}
+
+Code Example: 
+```
+functions:
+  sourceLambda:
+    handler: ./src/functions/dynamodbOperation
+    events:
+      - stream:
+          arn: ${fetchStreamARN(<tableName>)} // This is all the change you need to do
+          startingPosition: LATEST
+          enabled: true // Any other parameters can be added as provided by serverless
+```
+
+#### Note: Passing region where the table exists is important, the plugin automatically takes the value from serverless -> provider -> region
+
+#### This can be overwritten by passing region in command line property or
+#### Optionally you can add region of where to fetch dynamodb tables by passing it as the second parameter to the fetchStreamArn Function 
+
+##### arn: ${fetchStreamARN(<tableName>, <region>)}
---
+++ index.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+class fetchDynamoDBStreamsPlugin {
+	constructor(serverless, options) {
+		this.serverless = serverless;
+		this.options = options;
+		this.configurationVariablesSources = {
+			fetchStreamARN: {
+				async resolve({ address, params, resolveConfigurationProperty, __ }) {
+					let myStreamArn = null;
+					try {
+						if(!params || params.length < 1)
+							throw new Error("No table name passed to fetchStreamARN Function");
+						const configuredRegion =
+							await resolveConfigurationProperty([
+								"provider",
+								"region",
+							]);
+						let region = options.region ?? configuredRegion;
+						if(params.length > 1){
+							region = params[1]
+						}
+						const tableName = params[0];
+						serverless.cli.log(
+							`Fetching Streams of [ Table : ${tableName} ] in region ${region}`
+						);
+						const data = await getDynamoDBStreams(
+							region,
+							tableName
+						);
+
+						myStreamArn = extractStreamARNFromStreamData(data, tableName);
+						if (!myStreamArn)
+							throw new Error('Could not find stream of this Table');
+						else {
+							serverless.cli.log(
+								`Fetched stream of [ Table : ${tableName} ] => ${myStreamArn}`
+							);
+						}
+					} catch (err) {
+						const errorMessage =
+							err && err.message ? err.message : err;
+						serverless.cli.log(
+							`Error: ${JSON.stringify(errorMessage)}`
+						);
+					}
+
+					return {
+						value: myStreamArn,
+					};
+				},
+			},
+		};
+	}
+}
+
+const extractStreamARNFromStreamData = (data, tableName) => {
+	const Streams = data.Streams;
+	if (!Streams || Streams.length === 0) {
+		throw new Error(
+			`Cannot Find Stream of [ Table : ${tableName} ], make sure the table exist and stream is enabled`
+		);
+	}
+	const streamArn = Streams[0].StreamArn;
+	return streamArn;
+};
+
+const getDynamoDBStreams = async (region, tableName) => {
+	const dynamoStreams = new AWS.DynamoDBStreams({
+		region: region,
+	});
+	const params = {
+		TableName: tableName,
+	};
+	return dynamoStreams.listStreams(params).promise();
+};
+
+module.exports = fetchDynamoDBStreamsPlugin;
